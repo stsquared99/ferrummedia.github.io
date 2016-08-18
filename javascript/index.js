1a39a6480f373686877dc06b69be26af5cb97a00
Travis Cory <tcory@tcory-2.local>
2016-08-17 20:09:33 -0700

    Add email modal

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
 // JS
+var body = document.body;
+var modal = document.querySelector('.modal');
+var overlay = document.querySelector('.overlay');
+var submitButton = document.querySelector('.btn-update-button');
+
+var modalClose = modal.querySelector('.btn-close-modal');
+
+
+var toggleModal = function() {
+	console.log("click");
+
+
+	body.classList.toggle('modal-open');
+	modal.classList.toggle('hide');
+	overlay.classList.toggle('hide');
+
+	console.log(body.classList);
+}
+
+submitButton.addEventListener('click', toggleModal);
+modalClose.addEventListener('click', toggleModal);
