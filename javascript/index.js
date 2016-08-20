129525238ba9ca077f64b8c3a1e07a7f57084385
Travis Cory <tcory@tcory-2.local>
2016-08-19 18:27:22 -0700

    Add mailchimp form

--- javascript/index.js
+++ javascript/index.js
@@ -18,5 +18,23 @@ var toggleModal = function() {
 	console.log(body.classList);
 }
 
+// Close modal with esc key
+
+document.onkeydown = function(evt) {
+	evt = evt || window.event;
+	var isEscape = false;
+
+	if ("key" in evt) {
+	   	isEscape = evt.key == "Escape";
+	}
+	else {
+		isEscape = evt.keyCode == 27;
+	}
+
+	if (isEscape && body.classList.contains('modal-open')) {
+		toggleModal();
+	}
+};
+
 submitButton.addEventListener('click', toggleModal);
 modalClose.addEventListener('click', toggleModal);
